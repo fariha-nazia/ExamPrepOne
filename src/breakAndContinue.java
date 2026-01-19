58392d6c4bfcb0864926440a21413152acc40c5f
fariha-nazia <farihanazia@gmail.com>
2026-01-19 21:34:04 +0100

    init commit

--- src/breakAndContinue.java
+++ src/breakAndContinue.java
@@ -1,2 +1,34 @@
 public class breakAndContinue {
+    public static void main(String[] args){
+        /*
+        * Break and Continue
+        *  break = break out of a loop
+        *        = STOP
+        *  continue = skip current iteration of a loop
+        *           = SKIP
+        * */
+
+//        Let see how break works
+        for(int i = 0 ; i < 10 ; i++){
+            if(i == 5){
+                break;
+            }
+            System.out.println(i + " ");
+        }
+
+
+
+//        Let see how continue works
+        for(int i = 0 ; i < 10 ; i++){
+            if(i == 5){
+                continue;
+            }
+            System.out.println(i + " ");
+        }
+
+
+
+
+
+    }
 }
